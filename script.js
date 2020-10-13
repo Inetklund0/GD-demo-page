bf3b18341af55c527a9246db851e63b96774ec2c
Inetklund0 <inekielland97@gmail.com>
2020-10-13 10:33:54 +0200

    Add trackingcode

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 window.addEventListener('wheel', cup);
 
+let frame = 0;
+
 function cup (){
     // event.deltaY;
     
     let productElm = document.querySelector('.product-demo');
-    productElm.style.cssText = "border: 5px solid black;";
-
+    productElm.style.cssText = "background-position-x: calc("+Math.floor (frame) +" * -20px);";
+    frame += 0.1;
+    if (frame > 3) frame = 0;
 }
