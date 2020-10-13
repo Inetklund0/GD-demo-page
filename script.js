6048d20babe0ff1c3d123068f235a2c4520b5ddd
Inetklund0 <inekielland97@gmail.com>
2020-10-13 12:14:23 +0200

    button gtag

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.addEventListener('wheel', cup);
 
+
 let frame = 0;
 
 function cup (){
@@ -10,3 +11,16 @@ function cup (){
     frame += 0.1;
     if (frame > 3) frame = 0;
 }
+
+let button = document.querySelector('.button');
+button.addEventListener('mousedown',handleOrderClick);
+
+function handleOrderClick (event){
+    console.log('button was clicked');
+    gtag('event', 'Click link', {
+        'event_category': 'order test',
+        'event_label': 'order event happened'
+    });
+}
+
+
